dfdc61b5e8ded4eb733a354414c895a40dc73e15
antu41 <antudas444475@gmail.com>
2025-06-26 14:34:20 +0600

    Updated layout with Bootstrap and navigation bar

--- MiniAccountManagementSystem/obj/Debug/net8.0/MiniAccountManagementSystem.AssemblyInfo.cs
+++ MiniAccountManagementSystem/obj/Debug/net8.0/MiniAccountManagementSystem.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniAccountManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5391b48bb735961f16e64fa7fd747ff6e53cd380")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b40033c034b40ded5a7a8421034b9710eba5c109")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniAccountManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniAccountManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
